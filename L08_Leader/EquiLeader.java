d9b9fb880059271959886c09e072a480d922739e
AndyFromTaiwan <chuangp@student.unimelb.edu.au>
2020-07-26 01:43:47 +1000

    update

--- L08_Leader/EquiLeader.java
+++ L08_Leader/EquiLeader.java
@@ -1,8 +1,8 @@
 /**
  *  Tasks Details:
  *  2. EquiLeader (Easy)
- * Find the index S such that the leaders of the sequences A[0], A[1],
- * ..., A[S] and A[S + 1], A[S + 2], ..., A[N - 1] are the same.
+ *  Find the index S such that the leaders of the sequences A[0], A[1],
+ *  ..., A[S] and A[S + 1], A[S + 2], ..., A[N - 1] are the same.
  *  
  *  Task Score: 100%
  *  Correctness: 100%
